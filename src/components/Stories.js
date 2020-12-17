a553c0c8a3ff2f31b8be25f229943e4df969bab1
ivanklak <ivanklak17@gmail.com>
2020-12-17 16:41:14 +0300

    Add timer

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,25 +1,30 @@
 import React from "react";
 import s from "./Stories.module.css";
 import { NavLink } from "react-router-dom";
+import TimeAgo from "timeago-react";
 
 const Stories = ({ stories, storyIds }) => {
   return (
     <div className={s.stories}>
       {stories.map(item =>
         item ? (
-          <NavLink
-            to={`/news/${item.id}`}
-            key={item.id}
-            className={s.storiesItem}
-          >
-            <div className={s.storiesText}>
-              <div className={s.storiesTitle}>{item.title}</div>
-              <div className={s.description}>
-                {item.score} likes <span>{item.by}</span>
-                <span>{item.time}</span>
+          <div className={s.storiesText}>
+            <NavLink
+              to={`/news/${item.id}`}
+              key={item.id}
+              className={s.storiesItem}
+            >
+              <div className={s.text}>
+                <div className={s.storiesTitle}>{item.title}</div>
+                <div className={s.description}>
+                  {item.score} likes by <span>{item.by}</span> {" "}
+                  <span>
+                    <TimeAgo datetime={new Date(item.time * 1000).toISOString()} />
+                  </span>
+                </div>
               </div>
-            </div>
-          </NavLink>
+            </NavLink>
+          </div>
         ) : null
       )}
     </div>
